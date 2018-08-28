fa50371960eb964cc47dab77ede4b8f0b345f2b5
JayQi <jayqizone@gmail.com>
2018-08-28 18:15:39 +0800

    Update

--- index.js
+++ index.js
@@ -1,25 +1,86 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 44555 })
+const Exec = require('child_process').exec;
 
+const wss = new WebSocket.Server({ port: 44555 })
 wss.on('connection', (ws, req) => {
     ws.on('message', message => {
-        var data = deserialize(message);
+        let data = JSON.parse(message);
         let type = data.type;
         let args = data.args;
-        if (type === 'log') {
-            console.log(...args);
-        } else if (type === 'info') {
-            console.info(...args)
-        } else if (type === 'warn') {
-            console.warn(...args);
-        } else if (type === 'error') {
-            console.error(...args)
-        } else if (type === 'clear') {
-            console.clear()
+
+        if (type === 'clear') {
+            if (process.platform === 'darwin' && process.env.VSCODE_PID) {
+                vscode_clear(process.env.VSCODE_PID);
+            } else {
+                console.clear();
+            }
+        } else {
+            console[type](...args);
         }
     });
+
+    $ = cmd => ws.send(cmd) || '-'.repeat(78);
+    $.__proto__ = chain();
 })
 
-function deserialize(serializedJavascript) {
-    return eval('(' + serializedJavascript + ')');
+function chain(props = []) {
+    // 用于截获链式调用时有函数调用的形式，$.xxx.yyy(args).zzz，拼装参数
+    function func(...args) {
+        args = args || [];
+        let name = props.pop();
+        if (name === 'set') {
+            let setValue = typeof args[0] === 'string' ? args[0] : JSON.stringify(args[0])
+            let cmd = props.reduce((obj, prop) => `${obj}.${prop}`) + ' = ' + setValue;
+            return $(cmd);
+        } else {
+            return chain([...props, name + '(' + args.map(arg => JSON.stringify(arg)).join(',') + ')']);
+        }
+    }
+    func.__proto__ = new Proxy({ props }, {
+        get: function (target, key, receiver) {
+            if (key === '__proto__') {
+                return;
+            }
+            if (key === 'get') {
+                let cmd = props.reduce((obj, prop) => `${obj}.${prop}`);
+                return $(cmd);
+            } else {
+                if (key === '_proto_') {
+                    key = '__proto__'
+                }
+                return chain([...props, key]);
+            }
+        },
+        set: function (target, key, value, receiver) {
+            props.push(key);
+            let setValue = typeof value === 'string' ? value : JSON.stringify(value)
+            let cmd = props.reduce((obj, prop) => `${obj}.${prop}`) + ' = ' + setValue;
+            return $(cmd);
+        },
+        ownKeys(target) {
+            return;
+        }
+    });
+    return func;
+}
+
+// VS Code 控制台不支持 console.clear，可以先给清空控制台绑定快捷键 CTRL + L 然后通过 AppleScript 发达键盘事件触发
+function vscode_clear(pid) {
+    let script = `osascript -l JavaScript <<"EOF"\n
+ObjC.import('Carbon');
+
+let keyDown = $.CGEventCreateKeyboardEvent($(), 37, true);
+$.CGEventSetFlags(keyDown, $.kCGEventFlagMaskControl);
+
+let keyUp = $.CGEventCreateKeyboardEvent($(), 37, false);
+$.CGEventSetFlags(keyUp, $.kCGEventFlagMaskControl);
+
+$.CGEventPostToPid(${pid}, keyDown);
+delay(0.1);
+$.CGEventPostToPid(${pid}, keyUp);
+\nEOF
+`
+    Exec(script, (error, stdout, stderr) => {
+        error && console.error(error);
+    });
 }
--- jsboxModule/socketLogger.js
+++ jsboxModule/socketLogger.js
@@ -1,201 +1,49 @@
-var UID = Math.floor(Math.random() * 0x10000000000).toString(16);
-var PLACE_HOLDER_REGEXP = new RegExp('"@__(F|R|D)-' + UID + '-(\\d+)__@"', 'g');
-
-var IS_NATIVE_CODE_REGEXP = /\{\s*\[native code\]\s*\}/g;
-var UNSAFE_CHARS_REGEXP = /[<>\/\u2028\u2029]/g;
-
-// Mapping of unsafe HTML and invalid JavaScript line terminator chars to their
-// Unicode char counterparts which are safe to use in JavaScript strings.
-var ESCAPED_CHARS = {
-    '<': '\\u003C',
-    '>': '\\u003E',
-    '/': '\\u002F',
-    '\u2028': '\\u2028',
-    '\u2029': '\\u2029'
-};
-
-function escapeUnsafeChars(unsafeChar) {
-    return ESCAPED_CHARS[unsafeChar];
-}
-
-function fullSerialize(obj, options) {
-    options || (options = {});
-
-    // Backwards-compatibility for `space` as the second argument.
-    if (typeof options === 'number' || typeof options === 'string') {
-        options = { space: options };
-    }
-
-    var functions = [];
-    var regexps = [];
-    var dates = [];
-
-    // Returns placeholders for functions and regexps (identified by index)
-    // which are later replaced by their string representation.
-    function replacer(key, value) {
-        if (!value) {
-            return value;
-        }
-
-        // If the value is an object w/ a toJSON method, toJSON is called before
-        // the replacer runs, so we use this[key] to get the non-toJSONed value.
-        var origValue = this[key];
-        var type = typeof origValue;
-
-        if (type === 'object') {
-            if (origValue instanceof RegExp) {
-                return '@__R-' + UID + '-' + (regexps.push(origValue) - 1) + '__@';
-            }
-
-            if (origValue instanceof Date) {
-                return '@__D-' + UID + '-' + (dates.push(origValue) - 1) + '__@';
-            }
-        }
-
-        if (type === 'function') {
-            return '@__F-' + UID + '-' + (functions.push(origValue) - 1) + '__@';
-        }
-
-        return value;
-    }
-
-    var str;
-
-    // Creates a JSON string representation of the value.
-    // NOTE: Node 0.12 goes into slow mode with extra JSON.stringify() args.
-    if (options.isJSON && !options.space) {
-        str = JSON.stringify(obj);
-    } else {
-        str = JSON.stringify(obj, options.isJSON ? null : replacer, options.space);
-    }
-
-    // Protects against `JSON.stringify()` returning `undefined`, by serializing
-    // to the literal string: "undefined".
-    if (typeof str !== 'string') {
-        return String(str);
-    }
-
-    // Replace unsafe HTML and invalid JavaScript line terminator chars with
-    // their safe Unicode char counterpart. This _must_ happen before the
-    // regexps and functions are serialized and added back to the string.
-    if (options.unsafe !== true) {
-        str = str.replace(UNSAFE_CHARS_REGEXP, escapeUnsafeChars);
-    }
-
-    if (functions.length === 0 && regexps.length === 0 && dates.length === 0) {
-        return str;
-    }
-
-    // Replaces all occurrences of function, regexp and date placeholders in the
-    // JSON string with their string representations. If the original value can
-    // not be found, then `undefined` is used.
-    return str.replace(PLACE_HOLDER_REGEXP, function (match, type, valueIndex) {
-        if (type === 'D') {
-            return "new Date(\"" + dates[valueIndex].toISOString() + "\")";
-        }
-
-        if (type === 'R') {
-            return regexps[valueIndex].toString();
-        }
-
-        var fn = functions[valueIndex];
-        var serializedFn = fn.toString();
-
-        if (IS_NATIVE_CODE_REGEXP.test(serializedFn)) {
-            throw new TypeError('Serializing native function: ' + fn.name);
-        }
-
-        return serializedFn;
-    });
-}
-
-let msgQueue = []
-
-let startTimeer = function(socket) {
-    return $timer.schedule({
-        interval: 0.5,
-        handler: () => {
-            while (msgQueue.length > 0 && socket.readyState === 1) {
-                let msg = msgQueue.shift()
-                socket.send(msg)
-            }
-        }
-    })
-}
-
 module.exports = {
-    init: (address, port=44555, debug = true) => {
-        try {
-            var oldLog = console.log;
-            var oldInfo = console.info;
-            var oldWarn = console.warn;
-            var oldError = console.error;
-            var oldClear = console.clear;
-            var socket = $socket.new(`ws://${address}:${port}`);
-
-            let timer = startTimeer(socket)
-            
-            socket.listen({
-                didOpen: sock => {
-                    let checker = $timer.schedule({
-                        interval: 1,
-                        handler: () => {
-                            if (msgQueue.length === 0) {
-                                timer.invalidate()
-                                checker.invalidate()
-                            }
-                        }
-                    })
-                }
-            })
-
-            socket.open()
-    
-            console.log = function () {
-                if (debug) {
-                    let msg = fullSerialize({ type: 'log', args: Array.prototype.slice.call(arguments) })
-                    sendMessage(socket, msg);
-                }
-                oldLog.apply(console, arguments);
+    init: (address, port, clearFirst = true, debug = true) => {
+        var oldLog = console.log;
+        var oldInfo = console.info;
+        var oldWarn = console.warn;
+        var oldError = console.error;
+        let start = clearFirst
+        var socket = $socket.new(`ws://${address}:${port}`);
+        socket.open()
+        console.log = function (obj) {
+            if (debug) {
+                let msg = JSON.stringify({ type: 'log', args: Array.prototype.slice.call(arguments) });
+                start = sendMessage(start, socket, msg);
             }
-            console.info = function () {
-                if (debug) {
-                    let msg = fullSerialize({ type: 'info', args: Array.prototype.slice.call(arguments) });
-                    sendMessage(socket, msg);
-                }
-                oldInfo.apply(console, arguments);
+            oldLog.apply(console, arguments);
+        }
+        console.info = function (obj) {
+            if (debug) {
+                let msg = JSON.stringify({ type: 'info', args: Array.prototype.slice.call(arguments) });
+                start = sendMessage(start, socket, msg);
             }
-            console.warn = function () {
-                if (debug) {
-                    let msg = fullSerialize({ type: 'warn', args: Array.prototype.slice.call(arguments) });
-                    sendMessage(socket, msg);
-                }
-                oldWarn.apply(console, arguments);
+            oldInfo.apply(console, arguments);
+        }
+        console.warn = function (obj) {
+            if (debug) {
+                let msg = JSON.stringify({ type: 'warn', args: Array.prototype.slice.call(arguments) });
+                start = sendMessage(start, socket, msg);
             }
-            console.error = function () {
-                if (debug) {
-                    let msg = fullSerialize({ type: 'error', args: Array.prototype.slice.call(arguments) });
-                    sendMessage(socket, msg);
-                }
-                oldError.apply(console, arguments);
+            oldWarn.apply(console, arguments);
+        }
+        console.error = function (obj) {
+            if (debug) {
+                let msg = JSON.stringify({ type: 'error', args: Array.prototype.slice.call(arguments) });
+                start = sendMessage(start, socket, msg);
             }
-            console.clear = function() {
-                if (debug) {
-                    let msg = fullSerialize({ type: 'clear', args: [] });
-                    sendMessage(socket, msg)
-                }
-                oldClear.apply(console, arguments)
-            }
-        } catch(e) {
-            console.error('JSBox版本暂不支持远程调试！')
+            oldError.apply(console, arguments);
         }
     }
 }
 
-function sendMessage(socket, msg) {
-    if (socket.readyState === 1 && msgQueue.length === 0) {
-        socket.send(msg);
-    } else {
-        msgQueue.push(msg)
+function sendMessage(start, socket, msg) {
+    if (start) {
+        socket.send(JSON.stringify({ type: "_open" }));
+        start = false;
     }
+    socket.send(msg);
+    return start;
 }
+
