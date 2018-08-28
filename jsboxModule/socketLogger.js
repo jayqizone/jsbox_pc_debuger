27be54b195a2859ce2c2cea5eeada006fec4eafd
JayQi <jayqizone@gmail.com>
2018-08-28 18:30:34 +0800

    Update

--- jsboxModule/socketLogger.js
+++ jsboxModule/socketLogger.js
@@ -1,49 +1,46 @@
 module.exports = {
-    init: (address, port, clearFirst = true, debug = true) => {
-        var oldLog = console.log;
-        var oldInfo = console.info;
-        var oldWarn = console.warn;
-        var oldError = console.error;
-        let start = clearFirst
-        var socket = $socket.new(`ws://${address}:${port}`);
-        socket.open()
-        console.log = function (obj) {
-            if (debug) {
-                let msg = JSON.stringify({ type: 'log', args: Array.prototype.slice.call(arguments) });
-                start = sendMessage(start, socket, msg);
+    init: (address, { port = 44555, clear = true, debug = true } = { port: 44555, clear: true, debug: true }) => {
+        let origin = console.__proto__;
+        socket = $socket.new(`ws://${address}:${port}`);
+        socket.listen({
+            didOpen: (sock) => {
+                clear && socket.send(JSON.stringify({ type: 'clear' }));
+                origin.log.call(console, "Debugger attached.");
+            },
+            didFail: (sock, error) => {
+                origin.error.call(console, `Debugger detached With Error: ${error.localizedDescription}`);
+            },
+            didClose: (sock, code, reason, wasClean) => {
+                origin.log.call(console, `Debugger detached with reason: ${reason}`);
+            },
+            didReceiveString: (sock, cmd) => {
+                origin.log.call(console, `Inspect: ${cmd}`);
+                if (debug) {
+                    try {
+                        let result = new Function(`return eval(\`` + $addin.compile(cmd) + `\`)`)();
+                        socket.send(JSON.stringify({ type: 'log', args: [result] }));
+                        origin.log.call(console, result);
+                    } catch (error) {
+                        socket.send(JSON.stringify({ type: 'error', args: [error.message] }));
+                        origin.error.call(console, error.message);
+                    }
+                }
+            },
+            didReceiveData: (sock, data) => {
+                origin.log.call(console, `Received: ${data}`);
+            },
+            didReceivePing: (sock, data) => {
+                origin.log.call(console, "WebSocket received ping");
+            },
+            didReceivePong: (sock, data) => {
+                origin.log.call(console, "WebSocket received pong");
             }
-            oldLog.apply(console, arguments);
-        }
-        console.info = function (obj) {
-            if (debug) {
-                let msg = JSON.stringify({ type: 'info', args: Array.prototype.slice.call(arguments) });
-                start = sendMessage(start, socket, msg);
-            }
-            oldInfo.apply(console, arguments);
-        }
-        console.warn = function (obj) {
-            if (debug) {
-                let msg = JSON.stringify({ type: 'warn', args: Array.prototype.slice.call(arguments) });
-                start = sendMessage(start, socket, msg);
-            }
-            oldWarn.apply(console, arguments);
-        }
-        console.error = function (obj) {
-            if (debug) {
-                let msg = JSON.stringify({ type: 'error', args: Array.prototype.slice.call(arguments) });
-                start = sendMessage(start, socket, msg);
-            }
-            oldError.apply(console, arguments);
-        }
+        });
+        socket.open();
+
+        ['log', 'info', 'warn', 'error', 'clear'].forEach(type => console[type] = function (...args) {
+            debug && socket.send(JSON.stringify({ type, args }));
+            origin[type].apply(console, args);
+        });
     }
 }
-
-function sendMessage(start, socket, msg) {
-    if (start) {
-        socket.send(JSON.stringify({ type: "_open" }));
-        start = false;
-    }
-    socket.send(msg);
-    return start;
-}
-
