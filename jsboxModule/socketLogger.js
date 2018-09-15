02b0db60085df138eed827fb8b11671bc9c4f680
JayQi <jayqizone@gmail.com>
2018-09-15 11:49:41 +0800

    Optimization

--- jsboxModule/socketLogger.js
+++ jsboxModule/socketLogger.js
@@ -1,61 +1,75 @@
 module.exports = {
     init: (address, { port = 44555, clear = true, debug = true } = { port: 44555, clear: true, debug: true }) => {
-        let origin = console.__proto__;
+        let proto = console.__proto__;
+        let origin = {};
         socket = $socket.new(`ws://${address}:${port}`);
+
+        ['log', 'info', 'warn', 'error', 'clear'].forEach(type => {
+            origin[type] = proto[type].bind(console);
+            console[type] = (...args) => {
+                origin[type](...args);
+                debug && socket.send(JSON.stringify({ type, args: preSerialize(...args) }));
+            }
+        });
+
         socket.listen({
             didOpen: (sock) => {
-                origin.log.call(console, 'Debugger attached.');
+                origin.log('Debugger attached.');
                 clear && socket.send(JSON.stringify({ type: 'clear' }));
             },
             didFail: (sock, error) => {
-                origin.error.call(console, `Debugger detached With Error: ${error.localizedDescription}`);
+                origin.log(`Debugger detached With Error: ${error.localizedDescription}`);
             },
             didClose: (sock, code, reason, wasClean) => {
-                origin.log.call(console, `Debugger detached with reason: ${reason}`);
+                origin.log(`Debugger detached with reason: ${reason}`);
             },
             didReceiveString: (sock, cmd) => {
-                origin.log.call(console, `Inspect: ${cmd}`);
+                origin.log(`Inspect: ${cmd}`);
                 if (debug) {
                     try {
                         let result = new Function('return eval(`' + $addin.compile(cmd) + '`)')();
-                        origin.log.call(console, result);
+                        origin.log(result);
                         socket.send(JSON.stringify({ type: 'log', args: preSerialize(result) }));
                     } catch (error) {
-                        origin.error.call(console, error.message);
+                        origin.error(error.message);
                         socket.send(JSON.stringify({ type: 'error', args: [error.message] }));
                     }
                 }
             },
             didReceiveData: (sock, data) => {
-                origin.log.call(console, `Received: ${data}`);
+                origin.log(`Received: ${data}`);
             },
             didReceivePing: (sock, data) => {
-                origin.log.call(console, 'WebSocket received ping');
+                origin.log('WebSocket received ping');
             },
             didReceivePong: (sock, data) => {
-                origin.log.call(console, 'WebSocket received pong');
+                origin.log('WebSocket received pong');
             }
         });
-        socket.open();
 
-        ['log', 'info', 'warn', 'error', 'clear'].forEach(type => console[type] = function (...args) {
-            origin[type].apply(console, args);
-            debug && socket.send(JSON.stringify({ type, args: preSerialize(...args) }));
-        });
+        socket.open();
     }
 }
 
 function preSerialize(...args) {
-    return args.map(arg => {
-        if (arg instanceof Array) {
-            return preSerialize(...arg);
+    return args.map(arg => analyzeObject(arg));
+}
+
+function analyzeObject(obj) {
+    if (obj instanceof Array) {
+        return preSerialize(...obj);
+    }
+    if (obj instanceof Function) {
+        return `${obj}`;
+    }
+    if (obj instanceof Object) {
+        if (/\[object (?!Object)/.test(obj.toString())) {
+            return obj.runtimeValue().$description().rawValue();
+        } else {
+            for (let key in obj) {
+                obj[key] = analyzeObject(obj[key]);
+            }
         }
-        if (arg instanceof Function) {
-            return `${arg}`;
-        }
-        if (arg instanceof Object && /\[object (?!Object)/.test(arg.toString())) {
-            return arg.runtimeValue().$description().rawValue();
-        }
-        return arg;
-    })
+    }
+    return obj;
 }
